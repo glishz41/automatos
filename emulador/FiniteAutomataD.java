0dadafbd60c6527c5a58ea6a2747a97dbb14673f
André Oliveira <78551538+azuletto@users.noreply.github.com>
2022-11-03 13:56:00 -0300

    Mudanças de leitura
    - Agora as leituras e escritas são feitas através de .split
    
    - Os patterns são salvos primeiro em vetores, depois são passados para patterns do tipo FuncaoTransicao.
    
    - O novo pattern de estados finais agora contém a separação por "|", permitindo números maiores de transições como "20", "300", ...
    ---------------
    O que falta fazer
    
    - Colocar a leitura das entradas e utilizar os patterns pra definir A/R
    (Leitura, comparação e saída).

--- emulador/FiniteAutomataD.java
+++ emulador/FiniteAutomataD.java
@@ -1,72 +1,77 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class FiniteAutomataD extends Exception {
     final String type = "FAD";
     Integer estadoInicial;
-    ArrayList<String> alfabeto;
-    ArrayList<Integer> estadosFinais;
-    Map<Integer, String> mapaTransicoes = new HashMap<Integer, String>();
+    Integer estadosFinais[];
+    FuncaoTransicao[] pattern;
+
 
     public FiniteAutomataD() {
         this.estadoInicial = 0;
-        this.alfabeto = new ArrayList<String>();
-        this.estadosFinais = new ArrayList<Integer>();
-        mapaTransicoes = new HashMap<Integer, String>();
+        this.estadosFinais = new Integer[0];  
     }
+
             public FiniteAutomataD(File arquivo) throws Exception {
+
+                // verifica se o arquivo existe
                 if(arquivo == null||!arquivo.exists()||!arquivo.getName().endsWith(".txt")) {
                     throw new Exception("Arquivo inválido");
-                }
-                else {
+                } else {
 
                     // abre o arquivo de texto para leitura
                     try {
-                        BufferedReader buffRead = new BufferedReader(new FileReader("texto.txt"));
+                        BufferedReader textBuffer = new BufferedReader(new FileReader("texto.txt"));
                         String linha = "";
 
                     // le a primeira linha e verifica se o tipo de automato é válido
-                        linha = buffRead.readLine();
+                        linha = textBuffer.readLine();
                         if(!linha.contains("AFD")) {
                             System.out.println("Tipo de automato inválido");
-                            java.lang.System.exit(1);
                         }
-
                         else {
                             while (true) {
 
-                    //verifica se a linha não é vazia ou se já foi lida pela primeira etapa de verificação
-
+                                //verifica se a linha não é vazia ou se já foi lida pela primeira etapa de verificação
                                 if (linha != null && !linha.contains("AFD")) {
                                 this.estadoInicial = Integer.parseInt(linha);
-                                linha = buffRead.readLine();
+                                linha = textBuffer.readLine();
                                 
-                                for (int i = 0; i < linha.length(); i++) {
-                                estadosFinais.add(Integer.parseInt(linha.charAt(i)+""));
+                                String estados[] = linha.split("|");
+                                
+                                //corta a linha de estados finais e atribui ao vetor estadosFinais.
+                                for (int i = 0; i < estados.length; i++) {
+                                    estadosFinais[i] = Integer.parseInt(estados[i]);
                                 }
+                                linha = textBuffer.readLine();
+                                textBuffer.close();
+                                setMapaTransicao(linha);
                                 
-                                linha = buffRead.readLine();
-
-
-                                } else
-                                    break;
-                                linha = buffRead.readLine();
+                                } else {
+                                throw new Exception("Arquivo inválido");
                             }
-                            buffRead.close();
-                        }  
-                    }
-                        catch (Exception e) {
-                            System.out.println("Erro ao ler o arquivo");
                         }
+                    }   textBuffer.close();
+                }
+                        catch (Exception e) {
+                        System.out.println("Erro ao ler o arquivo");
+                        }
+                
                     //
                 }           
         }
 
-    public void setAutomato(String linha) throws Exception {
+    public void setMapaTransicao(String linha) {
+    
+    //seta as transicoes em patterns
+    String transicoes[];
+    transicoes = linha.split("\\|");
 
+    for (int i = 0; i < transicoes.length; i++) {
+        pattern[i].setPattern(transicoes[i]);
+    }
+        
     }
 }
--- emulador/FuncaoTransicao.java
+++ emulador/FuncaoTransicao.java
@@ -1,76 +1,12 @@
 public class FuncaoTransicao {
-    Object simbolo[];
-    Object conjuntoEstado[];
-    Object alfabeto[];
-    Object pattern[];
-    int estado = 0;
-    
-    public Object FuncaoTransicaoaAD(Object estadoAtual, Object simbolo[], Object conjuntoEstado[]) {
-    int limite = 0;
-    //verificação de tamanho (se estiver no final, para)
-    if(limite == simbolo.length) { 
-    return estadoAtual;
-    } else {
+Integer estadoAtual;
+Integer proxEstado;
+String simbolo;
 
-    if(simbolo[limite] == pattern[limite]) {
-    estadoAtual = conjuntoEstado[estado];
-    limite++;
-    estado++;
-    } else {
-    limite++;   
-    FuncaoTransicaoaAD(estadoAtual, simbolo, conjuntoEstado);
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   /*  for (int i = 0; i < pattern.length; i++) {
-        if (alfabeto[i] == simbolo[i] && pattern[i] == simbolo[i]) {
-            return proximoEstado;
-        } else {
-            return estado;
-        }
-        this.estado = proximoEstado;
-    }
-    else {
-
-    System.out.println("O símbolo "+simbolo+" não pertence ao alfabeto");
-    }
-    limite++;
-    FuncaoTransicaoaAD(estado, (Object[]) simbolo[limite], proximoEstado);
-            } */
-    }
-    
-
-    return null;
+public void setPattern(String pattern) {
+    String chars[] = pattern.split(",");
+    estadoAtual = Integer.parseInt(chars[0]);
+    simbolo = chars[1];
+    proxEstado = Integer.parseInt(chars[2]);
     }
 }
