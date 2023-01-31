1d8a81d57e899c0d74569ee56d9bd656cfd60149
André Oliveira <78551538+azuletto@users.noreply.github.com>
2023-01-31 17:47:46 -0300

    :arrow_up: updates
    Adição da máquina determinística

---
+++ maquinas/AFD.java
@@ -0,0 +1,75 @@
+package maquinas;
+
+import pattern.Patterns;
+
+public class AFD {
+    Integer estadosFinais[], estadoInicial, estadoAtual;
+    String dados[], type = "F";
+    Patterns patterns[];
+
+    public AFD(String[] specs, String[] input) {
+        //construtores da classe
+        this.dados = input;
+        String estadoInicial = specs[1];
+        this.estadoInicial = Integer.parseInt(estadoInicial);
+
+        String[] estadosFinais = specs[2].split(",");
+        this.estadosFinais = new Integer[estadosFinais.length];
+        for (int i = 0; i < estadosFinais.length; i++) {
+            this.estadosFinais[i] = Integer.parseInt(estadosFinais[i]);
+        }
+
+        String[] patterns = specs[3].split(";");
+        this.patterns = new Patterns[patterns.length];
+        for (int i = 0; i < patterns.length; i++) {
+            this.patterns[i] = new Patterns(patterns[i], type);
+        }
+    }
+
+    public Boolean run() {
+        //metodo que executa a maquina
+        System.out.println("Estado Inicial em -> "+estadoInicial);
+        for (int i = 0; i < dados.length; i++) {
+            String input = dados[i];
+            estadoAtual = estadoInicial;
+            
+            
+            for (int j = 0; j < input.length(); j++) {
+                estadoAtual = proximoEstado(estadoAtual, input.charAt(j));
+
+                //verifica se o estado atual eh nulo ou seja se há transição
+                System.out.println("O Estado eh -> "+estadoAtual+" ---- Simbolo: "+input.charAt(j));
+                if(estadoAtual == null) {
+                    System.out.println("REJEITADO POR NAO TER TRANSICAO");
+                    return false;
+                }
+            }
+        }
+        //verifica se o estado atual eh final        
+        if (isFinal(estadoAtual)) {
+            System.out.println("ACEITA");
+            return true;
+        } else {
+            System.out.println("REJEITA");
+            return false;
+        }
+    }
+    //faz o papel da função de trnasição
+    public Integer proximoEstado(Integer estadoAtual, Character simbolo) {
+        for (int i = 0; i < patterns.length; i++) {
+            if (patterns[i].estado == estadoAtual && patterns[i].simbolo.equals(simbolo.toString()) || simbolo == '_') {
+                return patterns[i].proximoEstado;
+            }
+        }
+        return null;
+    }
+    //método de verificação de estado final
+    public Boolean isFinal(Integer estadoAtual) {
+        for (int i = 0; i < estadosFinais.length; i++) {
+            if (estadosFinais[i] == estadoAtual) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
